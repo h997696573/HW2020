6c0c70e9d15357b73e5705d2f66e9794d16d7d9a
Zhuang xu <997696573@qq.com>
2020-06-16 21:15:14 +0800

    6.16

--- scripts.js
+++ scripts.js
@@ -1,10 +1,76 @@
-debugger
-function renderEditor(){
-    let inputEl = document.querySelector("#defult-todo-panel .todo-editor > input");
 
-    inputEl.onchange=(e) =>{
-        console.log("input change:",e);
-    }
+let tasks = []; 
+
+function renderEditor(){
+   let inputEl = document.querySelector("#default-todo-panel .todo-editor > input");
+
+  // inputEl.onchange = (e) => {
+  //     console.log("text，",e.target.value);
+      // console.log("input change:",e);
+  // };
+
+  let addTaks = ()=>{
+    let newTask ={
+        title:inputEl.value,
+        done: false,
+    };
+
+    inputEl.value= "";
+
+
+    tasks.push(newTask);
+
+    console.log("tasks:",tasks);
+
+    renderTaskItems();
+  };
+
+  inputEl.onkeypress= (e)=>{
+      if(e.key==="Enter"){
+          addTaks();
+      }
+  };
+
+  let addEl = document.querySelector("#default-todo-panel .todo-editor > button");
+  addEl.onclick =(e) =>{
+      console.log("add click");
+      addTaks();
+    
+  };
+}
+
+function renderTaskItems(){
+   let itemsEl = document.querySelector("#default-todo-panel .todo-items");
+
+   itemsEl.querySelectorAll("div").forEach((node)=>node.remove());
+
+   for(let i =0;i<tasks.length;i++){
+       let task =tasks[i];
+       let itemEl = document.createElement("div");
+
+       let doneEl =document.createElement("input");
+       doneEl.type = "checkbox";
+       itemEl.append(doneEl);
+
+       let titleEl =document.createElement("label");
+       titleEl.innerText = task.title;
+       itemEl.append(titleEl);
+
+       let cancelEl =document.createElement("button");
+       cancelEl.innerText = "X";
+       cancelEl.onclick=()=>{
+           tasks.splice(i,1);
+           renderTaskItems();
+       };
+       itemEl.append(cancelEl);
+
+       itemsEl.append(itemEl);
+
+
+
+   }
 }
 
 renderEditor();
+renderTaskItems();
+
